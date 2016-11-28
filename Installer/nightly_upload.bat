bdde7b5befe671c35b6722d63cd9272bea2c1cb9
Neproify <yamasaki3214@gmail.com>
2016-11-28 19:45:18 +0100

    Windows nightly upload.

---
+++ Installer/nightly_upload.bat
@@ -0,0 +1,7 @@
+for %%f in (IVNetwork-*.exe) do (
+	curl -F "version=1.0.0" -F "type=windows-client" -F "artifact=@%%f" -F "secret=%UPLOAD_SECRET%" http://nightly.ivnetwork.tk/upload.php
+)
+
+for %%f in (IVNetworkServer-*.exe) do (
+	curl -F "version=1.0.0" -F "type=windows-x86-server" -F "artifact=@%%f" -F "secret=%UPLOAD_SECRET%" http://nightly.ivnetwork.tk/upload.php
+)
