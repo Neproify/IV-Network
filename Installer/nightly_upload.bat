2e5e39f69a7bd4b98e18b3c7e420f09edd7e9ac8
Neproify <yamasaki3214@gmail.com>
2016-11-28 20:30:51 +0100

    Sleep between uploads on windows.

--- Installer/nightly_upload.bat
+++ Installer/nightly_upload.bat
@@ -2,6 +2,8 @@ for %%f in (IVNetwork-*.exe) do (
 	curl -F "version=1.0.0" -F "type=windows-client" -F "artifact=@%%f" -F "secret=%UPLOAD_SECRET%" http://nightly.ivnetwork.tk/upload.php
 )
 
+timeout /T 5
+
 for %%f in (IVNetworkServer-*.exe) do (
 	curl -F "version=1.0.0" -F "type=windows-x86-server" -F "artifact=@%%f" -F "secret=%UPLOAD_SECRET%" http://nightly.ivnetwork.tk/upload.php
 )
