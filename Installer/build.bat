3a7cf7df9e4f33d7b6f4a1b6359134eb050c4866
Neproify <yamasaki3214@gmail.com>
2016-11-28 16:12:56 +0100

    Include revision number in Windows Client Installer

--- Installer/build.bat
+++ Installer/build.bat
@@ -1,4 +1,8 @@
 @echo off
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-list --count HEAD`) DO (
+SET REVISION=%%F
+)
+
 cd nsis
-makensis.exe ../installer.nsi
+makensis.exe /DVERSION=1.0.0.DEV /DREVISION=%REVISION% ../installer.nsi
 cd ..
